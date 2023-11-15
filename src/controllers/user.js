c741e50e6bd2e17c836d31adbfac08035a27b60f
DJ2513 <djontiveros2728@gmail.com>
2023-11-15 10:59:51 -0600

    Minor changes

---
+++ src/controllers/user.js
@@ -0,0 +1,17 @@
+
+exports.getUsers = async function (req, res) {
+
+}
+
+exports.postUser = async function (req, res) {
+
+}
+
+exports.putUser = async function (req, res) {
+
+}
+
+exports.deleteUsers = async function (req, res) {
+
+}
+
